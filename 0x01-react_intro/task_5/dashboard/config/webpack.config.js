674d0fdbc782578a164a67d2e02234129d7c04e2
Tim Simms <tsimms@gmail.com>
2023-01-12 11:07:19 -0600

    task 9 -- install bable

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -4,26 +4,44 @@ module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
     path: path.resolve(__dirname, '../dist'),
+    filename: 'bundle.js',
   },
   devServer: {
-    static: path.join(__dirname, '../dist'),
+    static: path.resolve(__dirname, '../dist'),
     hot: true,
     compress: true,
   },
+  devtool: 'inline-source-map',
   module: {
     rules: [
       {
-        test: /\.css$/i,
+        test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-        use: ["file-loader", "image-webpack-loader"],
-      }
+        test: /\.(jpe?g|png|gif|svg)$/,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true,
+              disable: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
     ],
   },
-  devtool: 'inline-source-map',
 };
