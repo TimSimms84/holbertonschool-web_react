f37f316af7584c1fef5bd7d868186f0cd4e31536
Tim Simms <tsimms@gmail.com>
2023-01-17 15:57:56 -0600

    task 2 part 2

--- 0x03-React_component/task_2/dashboard/src/Notifications/NotificationItem.js
+++ 0x03-React_component/task_2/dashboard/src/Notifications/NotificationItem.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+// import { markAsRead } from './Notifications'
 
 
 
-const NotificationItem = ({ type, html, value }) => {
+const NotificationItem = ({ type, html, value, markAsRead }) => {
   if (value) {
-    return ( <li data-priority={type}>{value}</li> );
+    return ( <li onClick={markAsRead} data-priority={type}>{value}</li> );
   }
-  return ( <li data-priority={type} dangerouslySetInnerHTML={html} /> );
+  return ( <li onClick={markAsRead} data-priority={type} dangerouslySetInnerHTML={html} /> );
 }
 
 
@@ -15,12 +16,14 @@ NotificationItem.propTypes = {
   type: PropTypes.string,
   html: PropTypes.shape({ __html: PropTypes.string }),
   value: PropTypes.string,
+  markAsRead: PropTypes.func.isRequired,
 };
 
 NotificationItem.defaultProps = {
   type: 'default',
   html: {},
   value: '',
+  markAsRead: () => {},  
 };
   
 export default NotificationItem;
