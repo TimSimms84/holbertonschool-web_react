df1469e94b80abb859be8d8b3edb26ed96792bdc
Tim Simms <tsimms@gmail.com>
2023-01-22 18:08:03 -0600

    task 1 and two

--- 0x00-react_state/task_2/dashboard/src/Header/Header.test.js
+++ 0x00-react_state/task_2/dashboard/src/Header/Header.test.js
@@ -1,101 +1,23 @@
-import Header from './Header'
-import { shallow, mount } from 'enzyme';
 import React from 'react';
+import Header from './Header';
+import { shallow } from 'enzyme';
+import { assert } from 'chai';
 import { StyleSheetTestUtils } from 'aphrodite';
-import AppContext from '../App/AppContext';
 
+StyleSheetTestUtils.suppressStyleInjection();
 
-describe('<Header />', () => {
-  let contextValues = {};
-  beforeEach(() => {
-    contextValues = {
-      user: {
-        email: '',
-        password: '',
-        isLoggedIn: false
-      },
-      logOut: () => {}
-    };
-    StyleSheetTestUtils.suppressStyleInjection();
+describe('Header', () => {
+  it('Header renders without crashing', () => {
+    shallow(<Header />);
   });
-  afterEach(() => {
-    StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+
+  it('Header renders img tag', () => {
+    const wrapper = shallow(<Header />);
+    assert.equal(wrapper.find('img').length, 1);
   });
-  it("renders Header without crashing", () => {
-    mount(<Header />, {
-        wrappingComponent: AppContext.Provider,
-        wrappingComponentProps: {
-          value: {
-            user: contextValues.user,
-            logOut: contextValues.logOut
-          },
-        },
-      });
-  });
-  it("renders img tag", () => {
-    contextValues.user.isLoggedIn = true;
-    const SHeader = mount(<Header />, {
-      wrappingComponent: AppContext.Provider,
-      wrappingComponentProps: {
-        value: {
-          user: contextValues.user,
-          logOut: contextValues.logOut
-        },
-      },
-    });
-    expect(SHeader.find('header img').length).toEqual(1);
-  });
-  it("renders h1 tag", () => {
-    contextValues.user.isLoggedIn = true;
-    const SHeader = mount(<Header />, {
-      wrappingComponent: AppContext.Provider,
-      wrappingComponentProps: {
-        value: {
-          user: contextValues.user,
-          logOut: contextValues.logOut
-        },
-      },
-    });
-    expect(SHeader.find('header h1').length).toEqual(1);
-  });
-  it("Does not render logoutSection with default context values", () => {
-    const SHeader = mount(<Header />, {
-      wrappingComponent: AppContext.Provider,
-      wrappingComponentProps: {
-        value: {
-          user: contextValues.user,
-          logOut: contextValues.logOut
-        },
-      },
-    });
-    expect(SHeader.find('#logoutSection').length).toEqual(0);
-  });
-  it("Renders logoutSection with isLoggedIn true in context values", () => {
-    contextValues.user.isLoggedIn = true;
-    const SHeader = mount(<Header />, {
-      wrappingComponent: AppContext.Provider,
-      wrappingComponentProps: {
-        value: {
-          user: contextValues.user,
-          logOut: contextValues.logOut
-        },
-      },
-    });
-    expect(SHeader.find('#logoutSection').length).toEqual(1);
-  });
-  it("logOut function is called when clicking on link", () => {
-    contextValues.user.isLoggedIn = true;
-    contextValues.logOut = jest.fn();
-    const SHeader = mount(<Header />, {
-      wrappingComponent: AppContext.Provider,
-      wrappingComponentProps: {
-        value: {
-          user: contextValues.user,
-          logOut: contextValues.logOut
-        },
-      },
-    });
-    SHeader.find('#logoutSection a').simulate('click');
-    expect(contextValues.logOut).toHaveBeenCalled;
+
+  it('Header renders h1 tag', () => {
+    const wrapper = shallow(<Header />);
+    assert.equal(wrapper.find('h1').length, 1);
   });
 });
