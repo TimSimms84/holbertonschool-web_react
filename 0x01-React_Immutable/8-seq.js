e20fe0f9c65f1c9fd9cdd38d172bc563ada42177
Tim Simms <tsimms@gmail.com>
2023-01-24 14:51:18 -0600

    fix capitalization

--- 0x01-React_Immutable/8-seq.js
+++ 0x01-React_Immutable/8-seq.js
@@ -5,8 +5,8 @@ export default function printBestStudents(grades) {
   const bestStudents = Seq(grades).filter((student) => student.score >= 70);
   console.log(bestStudents.map((student) => ({
     score: student.score,
-    firstName: student.firstName,
-    lastName: student.lastName,
+    firstName: student.firstName.charAt(0).toUpperCase() + student.firstName.slice(1),
+    lastName: student.lastName.charAt(0).toUpperCase() + student.firstName.slice(1),
   })).toJS());
 }
 
