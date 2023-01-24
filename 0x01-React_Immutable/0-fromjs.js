5ceadaf2f724dc28d52268383f0012794b5731ed
Tim Simms <tsimms@gmail.com>
2023-01-23 18:25:22 -0600

    init repo

--- 0x01-React_Immutable/0-fromjs.js
+++ 0x01-React_Immutable/0-fromjs.js
@@ -7,10 +7,3 @@ export default function getImmutableObject(obj) {
     fromJS(obj)
   );
 }
-
-console.log(getImmutableObject({
-  fear: true,
-  smell: -1033575916.9145899,
-  wall: false,
-  thing: -914767132
-}));
