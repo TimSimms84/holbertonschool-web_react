d091d9ad64d6aaa7cdbe37a422cfe11033953651
Tim Simms <tsimms@gmail.com>
2023-01-28 20:16:38 -0600

    finish 7

--- 0x02-react_redux_action_creator_normalizr/task_7/dashboard/src/actions/uiActionCreators.js
+++ 0x02-react_redux_action_creator_normalizr/task_7/dashboard/src/actions/uiActionCreators.js
@@ -29,11 +29,41 @@ export function hideNotificationDrawer() {
   };
 }
 
+export function LoginSuccess() {
+  return {
+    type: types.LOGIN_SUCCESS,
+  };
+}
+
+export function LoginFailure() {
+  return {
+    type: types.LOGIN_FAILURE,
+  };
+}
+
+// // Async action creator that returns function that dispatches actions
+export function loginRequest(email, password) {
+  return async function(dispatch) {
+    dispatch(Login(email, password));
+    const response = await fetch('./login-success.json');
+    const json = await response.json();
+    if (json.status === 'ok') {
+      dispatch(LoginSuccess());
+    }
+    else {
+      dispatch(LoginFailure());
+    }
+  };
+}
+
 export const uiActions = {
   login: Login,
   logout: Logout,
   displayNotificationDrawer: displayNotificationDrawer,
-  hideNotificationDrawer: hideNotificationDrawer
+  hideNotificationDrawer: hideNotificationDrawer,
+  loginRequest: loginRequest,
+  LoginFailure: LoginFailure,
+  LoginSuccess: LoginSuccess,
 }
 
 export const boundUiActions = dispatch => bindActionCreators(uiActions, dispatch);
--- 0x02-react_redux_action_creator_normalizr/task_7/dashboard/src/actions/uiActionCreators.test.js
+++ 0x02-react_redux_action_creator_normalizr/task_7/dashboard/src/actions/uiActionCreators.test.js
@@ -1,38 +1,143 @@
-// import * as types from './uiActionCreators';
-import * as types from './uiActionCreators';
+import {
+  Login, loginRequest, Logout,
+  displayNotificationDrawer, hideNotificationDrawer,
+  LoginSuccess, LoginFailure
+} from './uiActionCreators';
 
-describe('uiActionCreators', () => {
-  it('confirm types.login returns correct object', () => {
-    const email = 'fake@mail.com';
-    const password = 'fakepassword';
+import fetchMock from 'fetch-mock';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+
+describe('Login action creator', () => {
+  it('returns the correct action object', () => {
+    const email = 'test@example.com';
+    const password = 'password';
     const expectedAction = {
       type: 'LOGIN',
       user: {
-        email,
-        password,
+        email: 'test@example.com',
+        password: 'password',
       },
     };
-    expect(types.Login(email, password)).toEqual(expectedAction);
+
+    expect(Login(email, password)).toEqual(expectedAction);
+  });
+});
+
+describe('loginRequest', () => {
+  let dispatch;
+  beforeEach(() => {
+    dispatch = jest.fn();
   });
 
-  it('confirm types.logout returns correct object', () => {
+  it('dispatches LOGIN_SUCCESS when the json status is ok', async () => {
+    global.fetch = jest.fn().mockResolvedValue({
+      json: () => Promise.resolve({ status: 'ok' }),
+    });
+    await loginRequest()(dispatch);
+    expect(dispatch).toHaveBeenCalledWith({
+      type: 'LOGIN_SUCCESS',
+    });
+  });
+
+  it('dispatches LOGIN_FAILURE when the json status is not ok', async () => {
+    global.fetch = jest.fn().mockResolvedValue({
+      json: () => Promise.resolve({ status: 'not ok' }),
+    });
+    await loginRequest()(dispatch);
+    expect(dispatch).toHaveBeenCalledWith({
+      type: 'LOGIN_FAILURE',
+    });
+  });
+});
+
+describe('Logout action creator', () => {
+  it('returns the correct action object', () => {
     const expectedAction = {
       type: 'LOGOUT',
     };
-    expect(types.Logout()).toEqual(expectedAction);
+
+    expect(Logout()).toEqual(expectedAction);
   });
-  
-  it('confirm types.displayNotificationDrawer returns correct object', () => {
+});
+
+describe('displayNotificationDrawer action creator', () => {
+  it('returns the correct action object', () => {
     const expectedAction = {
       type: 'DISPLAY_NOTIFICATION_DRAWER',
     };
-    expect(types.displayNotificationDrawer()).toEqual(expectedAction);
-  });
 
-  it('confirm types.hideNotificationDrawer returns correct object', () => {
+    expect(displayNotificationDrawer()).toEqual(expectedAction);
+  });
+});
+
+
+describe('hideNotificationDrawer action creator', () => {
+  it('returns the correct action object', () => {
     const expectedAction = {
       type: 'HIDE_NOTIFICATION_DRAWER',
     };
-    expect(types.hideNotificationDrawer()).toEqual(expectedAction);
+
+    expect(hideNotificationDrawer()).toEqual(expectedAction);
   });
+});
+
+describe('LoginSuccess action creator', () => {
+  it('returns the correct action object', () => {
+    const expectedAction = {
+      type: 'LOGIN_SUCCESS',
+    };
+
+    expect(LoginSuccess()).toEqual(expectedAction);
+  });
+});
+
+describe('LoginFailure action creator', () => {
+  it('returns the correct action object', () => {
+    const expectedAction = {
+      type: 'LOGIN_FAILURE',
+    };
+
+    expect(LoginFailure()).toEqual(expectedAction);
+  });
+});
+
+describe('loginRequest', () => {
+  afterEach(() => {
+    fetchMock.restore();
+  });
+
+  it('dispatches LoginSuccess when login is successful', async () => {
+    fetchMock.getOnce('./login-success.json', { body: { status: 'ok' } });
+
+    const store = mockStore();
+    const email = 'test@example.com';
+    const password = 'password';
+    const expectedActions = [
+      Login(email, password),
+      LoginSuccess()
+    ];
+
+    await store.dispatch(loginRequest(email, password));
+    expect(store.getActions()).toEqual(expectedActions);
+  });
+
+it('dispatches LoginFailure when login fails', async () => {
+    fetchMock.getOnce('./login-success.json', { body: { status: 'error' } });
+
+    const store = mockStore();
+    const email = 'test@example.com';
+    const password = 'password';
+    const expectedActions = [
+      Login(email, password),
+      LoginFailure()
+    ];
+
+    await store.dispatch(loginRequest(email, password));
+    expect(store.getActions()).toEqual(expectedActions);
+  });
+
 });
