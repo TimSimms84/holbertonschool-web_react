5a297ac6adc4afc66edf32500887caff99212c9a
Tim Simms <tsimms@gmail.com>
2023-01-30 16:41:59 -0600

    3 init

---
+++ 0x03-react_redux_reducer_selector/task_3/dashboard/src/reducers/uiReducer.test.js
@@ -0,0 +1,27 @@
+
+import { StyleSheetTestUtils } from 'aphrodite';
+import uiReducer from './uiReducer';
+import { DISPLAY_NOTIFICATION_DRAWER } from '../actions/uiActionTypes';
+import { initialState } from './uiReducer';
+
+StyleSheetTestUtils.suppressStyleInjection();
+
+
+describe('uiReducere', () => {
+  it('returns the initial state when no action is passed', () => {
+    expect(uiReducer(undefined, {})).toEqual(initialState);
+  });
+
+  it('returns the initial state when the action SELECT_COURSE is passed', () => {
+    expect(uiReducer(undefined, { type: 'SELECT_COURSE' })).toEqual(initialState);
+  });
+
+  it('changes isNotificationDrawerVisible to true when DISPLAY_NOTIFICATION_DRAWER action is passed', () => {
+    const action = {
+      type: DISPLAY_NOTIFICATION_DRAWER,
+    };
+    const newState = uiReducer(initialState, action);
+    expect(newState.get('isNotificationDrawerVisible')).toBe(true);
+  });
+
+});
